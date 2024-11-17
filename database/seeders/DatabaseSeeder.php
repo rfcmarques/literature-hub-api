9a2a0bc7fcf41566640a972a080264859c9b20c4
Rui Marques <a.marques1967@gmail.com>
2024-11-17 16:37:37 +0000

    feat: add password hashing for test user and call BookSeeder in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,11 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => bcrypt(env('TEST_USER_PASSWORD')),
+        ]);
+
+        $this->call([
+            BookSeeder::class,
         ]);
     }
 }
